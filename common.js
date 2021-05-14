ec22f4bf0f4feefa59c5e3a0d9effe849e1d18b5
Adrian Sieber <>
2021-05-14 10:31:01 +0000

    Start adding a backend

---
+++ common.js
@@ -0,0 +1,29 @@
+export function getQrCodeText(data = {}) {
+  if (!data.name) throw new Error('Name of receiver must be set')
+
+  if (!data.message) throw new Error('Message must be set')
+  if (data.message.length > 140) throw new Error('Message must not be longer than 140 characters')
+
+  if (!data.iban) throw new Error('IBAN must be set')
+  if (data.iban.length !== 22) throw new Error('IBAN must be 22 characters long')
+
+  if (data.amount <= 0) throw new Error('Amount must be a positive number')
+
+  return stripIndent`
+    BCD
+    002
+    1
+    SCT
+    ${// BIC needs to be set for some apps,
+      // even though it is overwritten by the IBAN anyways.
+      data.bic || 'AAAAAAAAAAA'
+    }
+    ${data.name}
+    ${data.iban}
+    EUR${data.amount}
+    ${/* Unused field "purpose" */ ''}
+    ${/* Unused field "reference" */ ''}
+    ${data.message}
+    ${/* Unused field "note to user" */ ''}
+  `
+}
--- server.js
+++ server.js
@@ -1,85 +1,56 @@
-import QRCode from 'qrcode'
-import { stripIndent } from 'common-tags'
+const QRCode = require('qrcode')
+const { stripIndent } = require('common-tags')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const db = require('./db')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(express.static('dist'))
+
+// app.get('/', (req, res) => {
+// 	res.sendFile(path.join(__dirname + '/index.html'))
+// })
+
+app.listen(3000, () => console.log('server started'))
+
+// const persons = JSON.parse(process.env['persons'])
+
+// persons.forEach(person => {
+  
+// })
 
 
-function getQrCodeText({
-  name = '',
-  // BIC needs to be set for some apps,
-  // even though it is overwritten by the IBAN anyways.
-  bic = 'AAAAAAAAAAA',
-  iban = '',
-  amount = '',
-  message = '',
-}) {
-  return stripIndent`
-    BCD
-    002
-    1
-    SCT
-    ${bic}
-    ${name}
-    ${iban.replace(/\s/g, '')}
-    EUR${amount}
+// function displayQrCode () {
+//   const qrCodeText = getQrCodeText({
+//     name: data.name || data.firstname + ' ' + data.lastname,
+//     bic: data.bic,
+//     iban: data.iban,
+//     amount: data.amount,
+//     message: data.message,
+//   })
 
+//   console.log(request.headers)
 
-    ${message}
-  `
-}
+//   if (request.headers.accept.includes('image/png')) {
+//     response.setHeader('Content-Type', 'image/png')
 
-
-function displayQrCode () {
-  const qrCodeText = getQrCodeText({
-    name: data.name || data.firstname + ' ' + data.lastname,
-    bic: data.bic,
-    iban: data.iban,
-    amount: data.amount,
-    message: data.message,
-  })
-
-  console.log(request.headers)
-
-  if (request.headers.accept.includes('image/png')) {
-    response.setHeader('Content-Type', 'image/png')
-
-    qrcode.toFileStream(
-      response,
-      qrCodeText,
-      {
-        errorCorrectionLevel: 'M',
-        // version: 13, // Maximum allowed version (should be less normally)
-      }
-    )
-  }
-  else {
-    response.end(qrCodeText)
-    return
-  }
-}
-
-
-document.addEventListener('submit', event => {
-  event.preventDefault()
-  event.stopPropagation()
-
-  const formData = new FormData(event.srcElement)
-
-  const qrCodeText = getQrCodeText({
-    name: formData.get('name') ||
-      formData.get('firstname') + ' ' + formData.get('lastname'),
-    bic: formData.get('bic'),
-    iban: formData.get('iban'),
-    amount: formData.get('amount'),
-    message: formData.get('message'),
-  })
-
-  const canvas = document.getElementById('canvas')
-
-  QRCode.toCanvas(
-    canvas,
-    qrCodeText,
-    (error) => {
-      if (error) console.error(error)
-      else console.info('Success')
-    },
-  )
-})
+//     qrcode.toFileStream(
+//       response,
+//       qrCodeText,
+//       {
+//         errorCorrectionLevel: 'M',
+//         // version: 13, // Maximum allowed version (should be less normally)
+//       }
+//     )
+//   }
+//   else {
+//     response.end(qrCodeText)
+//     return
+//   }
+// }
